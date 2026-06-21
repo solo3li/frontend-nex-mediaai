c50892fe2e362a607bd7e4193d284d83d9ca200f
solo3li <hamed3alii.3@gmail.com>
2026-06-21 12:32:46 +0000

    production

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './src/i18n/routing';
- 
+import { routing } from './src/i18n/routing';
+
 export default createMiddleware(routing);
- 
+
 export const config = {
   matcher: ['/', '/(ar|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
+
+
